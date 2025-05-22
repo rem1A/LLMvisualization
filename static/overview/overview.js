416b0548fb8b62fbe59a25d30a45269b434aeb3a
Huang Xin <huangxin@HuangnoMacBook-Pro.local>
2025-05-23 08:57:21 +0900

    5.23latest

--- static/overview/overview.js
+++ static/overview/overview.js
@@ -8,8 +8,8 @@ class Overview extends BasicView
         super(container);
 
         this.dataManager = new OverviewData();
-
-
+        this.tooltip = null;
+        
         subscribe('data', this.update.bind(this));//gpt: 让 update() 方法内部能正确使用 this
     }
 
@@ -26,7 +26,20 @@ class Overview extends BasicView
         this.margin.left = 50;
         this.margin.top = 50;
 
-    }
+        this.tooltip = this.svg
+            .append('div')
+            .attr('class', 'tooltip')
+            .style('position', 'absolute')//绝对定位 用于确定tooltip的位置
+            .style('visibility', 'hidden')//设置tooltip的可见性 默认不显示 需要的时候再显示
+            .style('background', 'white')//背景颜色
+            .style('border', '1px solid black')
+            .style('padding', '5px')//内边距
+            .style('border-radius', '5px')//圆角
+            .style('font-size', '12px')
+            .style('pointer-events', 'none');//GPT: 禁止鼠标事件作用于 tooltip 本身。这样鼠标移到 tooltip 上时不会触发其他事件（如 tooltip 消失），避免干扰。
+
+        
+    }   
 
     draw(){
       
@@ -53,32 +66,21 @@ class Overview extends BasicView
             .attr('width', 20)
             .attr('height', 20)
             .attr('fill', d=> colorScale(d.total_rate))//gpt code: use colorScale to set the color
-            .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);//平移
-        
-        // this.svg.append('g').selectAll('.rect')
-        //     .data(this.dataManager.data)
-        //     .enter()
-        //     .append('rect')
-        //     .attr('x', (d, i)=>{
-        //         return i * 5;
-        //     } )
-        //     .attr('y', (d, i) =>{
-
-        //         if(i % 5 == 0)
-        //             return 0;
-        //         else if(i % 5 == 1)
-        //             return 50;
-        //         else if(i % 5 == 2)
-        //             return 100;
-        //         else if(i % 5 == 3)
-        //             return 150;
-        //         else
-        //             return 200;
-        //     })
-        //     .attr('width', 4)
-        //     .attr('height', 4)
-        //     .attr('fill', 'steelblue')
-        //     .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+            .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)//平移
+            .on('mouseover', (event, d) => {
+                this.tooltip.html(`Tensor Name: ${d.filename}<br>Tensor Size: ${d.shape[0]}<br>Tensor Rate: ${d.total_rate.toFixed(4)}`)
+                    .style('visibility', 'visible');
+                    console.log(d.filename);//test
+            })
+            .on('mousemove', (event, d) => {
+                this.tooltip.style('top', (event.pageY - 10) + 'px')
+                    .style('left', (event.pageX + 10) + 'px');
+                    console.log('mousemove');//test
+            })//鼠标移动时tooltip跟随鼠标
+            .on('mouseout', (event, d) => {
+                this.tooltip.style('visibility', 'hidden');
+                console.log('mouseout');//test
+            });//鼠标移出时tooltip隐藏
 
     }
 
